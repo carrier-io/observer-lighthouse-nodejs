45fed136b08704ffb27040023422bc4330bd6a19
kilerogosha <117376407+kilerogosha@users.noreply.github.com>
2025-12-24 16:33:54 +0300

    Add INP, TTFB and browser_version

--- util.py
+++ util.py
@@ -60,9 +60,9 @@ def upload_file(file_name, file_path, galloper_url, project_id, token, s3_config
         print(format_exc())
 
 
-def update_summary_file(report_id, records):
+def update_summary_file(report_id, records, browser_version="not_set"):
     file_exists = os.path.exists(f"/tmp/{report_id}.csv")
-    header = "timestamp,name,identifier,type,loop,load_time,dom,tti,fcp,lcp,cls,tbt,fvc,lvc,file_name\n".encode('utf-8')
+    header = "timestamp,name,identifier,type,loop,load_time,dom,tti,fcp,lcp,cls,tbt,fvc,lvc,inp,ttfb,file_name,browser_version\n".encode('utf-8')
     with open(f"/tmp/{report_id}.csv", 'ab+') as f:
         if not file_exists:
             f.write(header)
@@ -74,7 +74,8 @@ def update_summary_file(report_id, records):
                 f"{each['metrics']['largest_contentful_paint']},"
                 f"{each['metrics']['cumulative_layout_shift']},{each['metrics']['total_blocking_time']},"
                 f"{each['metrics']['first_visual_change']},{each['metrics']['last_visual_change']},"
-                f"{each['file_name']}\n".encode('utf-8'))
+                f"{each['metrics']['interaction_to_next_paint']},{each['metrics']['time_to_first_byte']},"
+                f"{each['file_name']},{browser_version}\n".encode('utf-8'))
 
 
 def get_summary_file_lines(report_id):
